f43bb9a42e56084bb48c98d23bd2e46e7e011675
David Bradway <david.bradway@gmail.com>
2024-02-23 20:13:12 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Hit Bull Win Steak
+
+Get the score, outcome, and date of most recent Durham Bulls game from a betting site.
+
+## Set up new conda enviroment with Python 3.11
+```bash
+conda create -n hitbullwinsteak python=3.11
+conda activate hitbullwinsteak
+conda install requests beautifulsoup4
+
+python get_score.py
+```
---
+++ get_score.py
@@ -0,0 +1,59 @@
+from bs4 import BeautifulSoup
+import requests
+import pickle
+import datetime
+
+# import the old score from the pickle file
+try:
+    with open('score.pkl', 'rb') as f:
+        (old_dt, old_outcome, old_score) = pickle.load(f)
+except:
+    old_dt = datetime.datetime(2024, 1, 1)
+    old_outcome = True
+    old_score = [0, 0]
+
+url = 'https://betsapi.com/t/4574/Durham-Bulls'
+page = requests.get(url)
+
+# create a beautiful soup object
+soup = BeautifulSoup(page.content, 'html.parser')
+
+# find the table
+table = soup.find('table', class_='table table-sm')
+
+# find the first row
+row = table.find('tr')
+
+# find the date column
+date = row.find('td', class_='dt_n')
+
+# get date from the data-dt attribute and convert from ISO-8601 to a datetime
+dt = datetime.datetime.fromisoformat(date.attrs['data-dt'])
+
+# find the second to last column in the row
+column = row.find_all('td')[-2]
+
+# get the text of the link in that column and strip whitespace
+link = column.find('a').text.strip()
+
+# split into a list of strings by the character '-'
+score_str = link.split('-')
+
+# convert the list of strings to a list of integers
+score = [int(x) for x in score_str]
+
+# find outcome
+column = row.find_all('td')[-3]
+
+# Beautilful soup select contents of the td tag
+outcome = column.contents[0]
+iswin = outcome == 'W'
+
+# if new date posted,
+if old_dt != dt:
+    print('Score:', score, 'is win:', iswin, 'Date:', dt)
+
+    # save a list to a pickle file
+    with open('score.pkl', 'wb') as f:
+        my_tuple = (dt, iswin, score)
+        pickle.dump(my_tuple, f)
